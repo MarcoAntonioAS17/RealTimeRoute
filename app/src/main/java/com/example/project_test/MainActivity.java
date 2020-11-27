1e7e9f3112b85b8554feaba78cd3869f051817c2
MarcoAntonioAS17 <viajero31173@innovaccion.mx>
2020-11-26 23:11:33 -0600

    Test del Proyecto

---
+++ app/src/main/java/com/example/project_test/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.project_test;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab_gps = findViewById(R.id.fab_gps);
+        fab_gps.setOnClickListener(view ->
+                Snackbar.make(view, "GPS Float Action Button", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        );
+
+        FloatingActionButton fab_go = findViewById(R.id.fab_go);
+        fab_go.setOnClickListener(view ->
+                Snackbar.make(view,"GO Float Action Button", Snackbar.LENGTH_LONG)
+                .setAction("Action",null).show()
+        );
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_routes, R.id.nav_real_time_routes)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
